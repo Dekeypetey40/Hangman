3f9404cd2ec389c11d5d4fcd3c3c47fba257e52a
Dekeypetey40 <kmichaelmikhail@gmail.com>
2023-08-01 13:27:17 +0200

    Write hangman rules

--- run.py
+++ run.py
@@ -1,2 +1,32 @@
-print("Hello")
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import statements
+import random
+import art
+
+
+
+
+#Constants
+
+def game_rules():
+    """
+    Show the user the game rules and how to play
+    """
+    #clear_screen()
+    print(
+        f"""
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    The objective is to save the man being hung by guessing the word.
+    You may either guess a letter or a full word.
+    Guess incorrectly and add a mistake.
+    Mistakes lead the man closer to death.
+    Guess the word correctly without making too many mistakes
+    and the you will save the man!
+
+
+    Will you be able to save the day?
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+        """
+    )
+    input("Press enter to return to main menu\n")
+game_rules()
