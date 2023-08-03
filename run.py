4fba9640826d069cebf56a11ff7c55aaf40f4ca2
Kai <kmichaelmikhail@gmail.com>
2023-08-03 13:11:03 +0200

    Write else statement if letter guessed is right

--- run.py
+++ run.py
@@ -5,6 +5,78 @@ from art import *
 from simple_term_menu import TerminalMenu
 
 
+#Art for hangman game taken from Crishorton https://gist.github.com/chrishorton/8510732aa9a80a03c829b09f12e20d9c
+HANGMANPICS = ['''
+  +---+
+      |
+      |
+      |
+      |
+      |
+=========
+7 GUESSES LEFT''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+6 GUESSES LEFT''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+5 GUESSES LEFT''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+4 GUESSES LEFT''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+3 GUESSES LEFT''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+2 GUESSES LEFT''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+1 GUESSES LEFT''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+YOU LOST!''']
+def hangman_art(lives):
+  return (HANGMANPICS[-lives-1])
+
+#return hangman_lives[-lives-1]
+
+
 
 
 
@@ -27,7 +99,7 @@ def main():
 def choose_difficulty():
     while True:
         print("Select difficulty level")
-        options = ["Easy: 9 guesses", "Medium: 7 guesses", "Hard: 5 guesses"]
+        options = ["Easy: 7 guesses", "Medium: 6 guesses", "Hard: 5 guesses"]
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
         if options[menu_entry_index] == "Easy: 7 guesses":
@@ -47,35 +119,66 @@ def choose_word():
 
 
 def play_game(word, lives):
-    guesses = []
-    hidden_word = [" _ "] * len(word)
+    guessed_letters = []
+    guessed_words = []
+    hidden_word = []
     game_over = False
-    print(f"\n\nThe word has {len(word)} letters.")  
     
+      
     for letter in word:     
-        print('_ ', end=' ')
-            
+        hidden_word.append("_")
     while not game_over and lives > 0:
-        
+        print(hangman_art(lives))
+        print (*hidden_word)
         print(word)
-        guess = input(f"Guess a letter \n")
-        #code partially taken from stack overflow to replace _ with correct letter
-        
-        for i, letter in enumerate(word):
-            if letter != "_" and guess == letter:
-                print("You have guessed correctly!")
-                hidden_word[i] = letter
-                guesses.append(guess)
-                print(guesses)
-                print("".join(hidden_word))
-        if guess in guesses:
-            print(f"You have already guessed {guess}, try again!")
-        elif guess not in word:
-            lives -=1
-            print(f"{guess} is not in the word!\nYou have {lives} lives remaining.")
+        print(f"\n\nThe word has {len(word)} letters.")
+        print("Letters guessed: " + ', '.join(guessed_letters))
+        guess = input("Guess a letter or word:\n").lower()
+        #code if user guesses a single letter
+        if guess.isalpha() and len(guess) == 1:
+            if guess in guessed_letters:
+                print(f"You have already guessed {guess}, try again!")
+            elif guess not in word:
+                print(f"{guess} is not in the word")
+                lives -=1
+                guessed_letters.append(guess)
+            else:
+                #Replacing underscores with correct letters inspired by https://www.youtube.com/watch?v=DLurhc1i5_4&ab_channel=MikhaHarly
+                print(f"""{guess} is in the word! Good Job!""")
+                guessed_letters.append(guess)
+                for position in range (len(word)):
+                    letter = word[position]
+                    if letter == guess:
+                        hidden_word[position] = letter
+                
+                
+                # print("You have guessed correctly!")
+                # guessed_letters.append(guess)
+                # hidden_list = list(hidden_word)
+                # letter_check = [i for i, letter in enumerate(word)
+                #            if letter != "_" and guess == letter]
+                # for check in letter_check:
+                #     hidden_list = [check] = guess
+                # hidden_word = "".join(hidden_list)
+                if "_" not in hidden_word:
+                    game_over = True
+            
+    #     guess = input(f"Guess a letter \n")
+    #     #code partially taken from stack overflow to replace _ with correct letter
+    #     print(hangman_art(lives))
+    #     for i, letter in enumerate(word):
+    #         if letter != "_" and guess == letter:
+    #             print("You have guessed correctly!")
+    #             hidden_word[i] = letter
+    #             guessed_letters.append(guess)
+    #             print("".join(hidden_word))
+    #     if guess in guessed_letters:
+    #         print(f"You have already guessed {guess}, try again!")
+    #     elif guess not in word:
+    #         lives -=1
+            
 
     
-
 def game_rules():
     """
     Show the user the game rules, which explain how to play.
@@ -104,10 +207,8 @@ def game_rules():
         main()
     #elif options[menu_entry_index] == "Quit":
         #game_rules()
-#main()
-word = choose_word()
-lives = 7
-play_game(word, lives)
+main()
+
 
 
 
